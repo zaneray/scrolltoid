c9d3408bf1f74f064fbe9c88cde63e29d14737b6
sebbyt16 <thomas.sebby@zaneray.com>
2015-11-04 16:27:39 -0700

    Update scrollToId to make it work as a plugin. Pretty sure the closing ) was in the wrong spot.

--- scrolltoid.js
+++ scrolltoid.js
@@ -8,27 +8,29 @@ accepts 'speed' and 'offset' arguments.
 
 (function ($) {
   $.fn.scrollToID = function (args) {
-  var options = {
-    speed:        500, 
-    offset:       20 
+    var options = {
+      speed:        500, 
+      offset:       20 
+    };
+   
+    $.extend( options, args );
+    
+    this.off('click.scrolltoid').on('click.scrolltoid',function(e){
+          
+      e.preventDefault();
+
+      var url       = $(this).attr('href'),
+          urlArray  = url.split('#'),
+          id        = urlArray[1],
+          topOffset = $('#' + id).offset().top - options.offset;
+
+      $('body, html').animate({
+        scrollTop: topOffset
+      },options.speed);
+
+    });
+
+    return this;
+
   };
- 
-  $.extend( options, args );
-  
-  this.off('click.scrolltoid').on('click.scrolltoid',function(e){
-        
-    e.preventDefault();
-
-    var url       = $(this).attr('href'),
-        urlArray  = url.split('#'),
-        id        = urlArray[1],
-        topOffset = $('#' + id).offset().top - options.offset;
-
-    $('body, html').animate({
-      scrollTop: topOffset
-    },options.speed);
-
-  });
-
-  };
-})( jQuery );
+}( jQuery ));
